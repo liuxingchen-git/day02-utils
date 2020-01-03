f10080f7e48057edefc60efee36dcb732c8d8c76
liuxingchen <1781485529@qq.com>
2020-01-03 19:22:18 +0800

    第四版！

---
+++ src/main/java/com/liuxingchen/util/NumberUtil.java
@@ -0,0 +1,22 @@
+package com.liuxingchen.util;
+/**
+ * 
+ * @ClassName: NumberUtil 
+ * @Description: TODO
+ * @author: HP
+ * @date: 2020年1月3日 下午7:18:06
+ */
+public class NumberUtil {
+	/**
+	 * 
+	 * @Title: isNumber 
+	 * @Description: 判断是否是数字
+	 * @param src
+	 * @return
+	 * @return: boolean
+	 */
+	public static boolean isNumber(String src) {
+		String reg = "^(-)?[0-9]+(\\.[0-9]+)?$";
+		return src.matches(reg);
+	}
+}
